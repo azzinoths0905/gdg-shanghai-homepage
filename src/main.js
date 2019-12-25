6ce1eb2c9ad3cda920fdf83c274192bc7ac06e40
Ron You <youyihao@gmail.com>
2019-12-26 03:32:34 +0800

    feat: add basic UI layout

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+// import './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import './registerServiceWorker'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import './style.css'
+import './filters/truncate'
+import vuetify from './plugins/vuetify';
+
+Vue.use(VueAxios, axios)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#518FF5',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
+  iconfont: 'md',
+})
+
+export default new Vuetify({
+});

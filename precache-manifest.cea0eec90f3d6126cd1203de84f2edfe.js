985ac03d77bf730f163486a38fb54169a3e92c00
Ron You <youyihao@gmail.com>
2020-01-19 22:19:30 +0800

    deploy

---
+++ precache-manifest.cea0eec90f3d6126cd1203de84f2edfe.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d1e8e12190b86d71f58",
+    "url": "/gdg-shanghai-homepage/css/app.9529a0dc.css"
+  },
+  {
+    "revision": "5d1e8e12190b86d71f58",
+    "url": "/gdg-shanghai-homepage/js/app.c9509908.js"
+  },
+  {
+    "revision": "127f26845af8e44558c5",
+    "url": "/gdg-shanghai-homepage/js/chunk-04d2dfca.ce132ea5.js"
+  },
+  {
+    "revision": "79e9c8d9fe74c63ab8c3",
+    "url": "/gdg-shanghai-homepage/css/chunk-590f2d58.c1431dd4.css"
+  },
+  {
+    "revision": "79e9c8d9fe74c63ab8c3",
+    "url": "/gdg-shanghai-homepage/js/chunk-590f2d58.f0c21c85.js"
+  },
+  {
+    "revision": "789f69bb643844ba634f",
+    "url": "/gdg-shanghai-homepage/js/chunk-62fcdc82.72583176.js"
+  },
+  {
+    "revision": "00b02db87cf39b2a90bd",
+    "url": "/gdg-shanghai-homepage/js/chunk-96e4e4fc.b7df918f.js"
+  },
+  {
+    "revision": "9583f6d9829c132f619d",
+    "url": "/gdg-shanghai-homepage/css/chunk-vendors.fd7140fa.css"
+  },
+  {
+    "revision": "9583f6d9829c132f619d",
+    "url": "/gdg-shanghai-homepage/js/chunk-vendors.f3e322c0.js"
+  },
+  {
+    "revision": "e2ff2fe83e32e79bcd0dad67228f342e",
+    "url": "/gdg-shanghai-homepage/img/aog.e2ff2fe8.png"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/gdg-shanghai-homepage/fonts/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "d65d8ad0385c2de9f5db95737cc2147a",
+    "url": "/gdg-shanghai-homepage/img/gdg.d65d8ad0.jpg"
+  },
+  {
+    "revision": "d192dc79598b0a98ade7e9a95569f451",
+    "url": "/gdg-shanghai-homepage/img/tf.d192dc79.svg"
+  },
+  {
+    "revision": "bccc0753b8e58526f7b5ac27bf5f0626",
+    "url": "/gdg-shanghai-homepage/img/IWD.bccc0753.jpg"
+  },
+  {
+    "revision": "0229df0933f2c59de929cb6bd2bdfcc3",
+    "url": "/gdg-shanghai-homepage/img/logo.0229df09.png"
+  },
+  {
+    "revision": "f06243561858f4728a136f75b5cad9c2",
+    "url": "/gdg-shanghai-homepage/img/polymer.f0624356.svg"
+  },
+  {
+    "revision": "48ae79d99d69739a8022593da1ece32f",
+    "url": "/gdg-shanghai-homepage/img/angular.48ae79d9.svg"
+  },
+  {
+    "revision": "f8fb99f2d34c78761130651f9d46c5ed",
+    "url": "/gdg-shanghai-homepage/img/gdg-x.f8fb99f2.svg"
+  },
+  {
+    "revision": "a711d8f75de4426eb710b3b8b1db5d7b",
+    "url": "/gdg-shanghai-homepage/img/gcp.a711d8f7.svg"
+  },
+  {
+    "revision": "c09e2e7ff071b9734f90997c88f10ef7",
+    "url": "/gdg-shanghai-homepage/img/gdg.c09e2e7f.png"
+  },
+  {
+    "revision": "710b4d5e665c80482063cdc5e4659bae",
+    "url": "/gdg-shanghai-homepage/img/wtm.710b4d5e.png"
+  },
+  {
+    "revision": "b58324f818e1e0a17f0132dc436f265f",
+    "url": "/gdg-shanghai-homepage/img/gdgjalandhar.b58324f8.png"
+  },
+  {
+    "revision": "c1b038098ab7760bbb1686c8e57ba8fb",
+    "url": "/gdg-shanghai-homepage/img/contact2.c1b03809.svg"
+  },
+  {
+    "revision": "d5a816ad5b18ea1be88e42db4014dd5f",
+    "url": "/gdg-shanghai-homepage/img/avatar.d5a816ad.png"
+  },
+  {
+    "revision": "7ef974da3894a3fc56d3e67f04a049a2",
+    "url": "/gdg-shanghai-homepage/img/bg.7ef974da.svg"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/gdg-shanghai-homepage/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/gdg-shanghai-homepage/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/gdg-shanghai-homepage/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/gdg-shanghai-homepage/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "14ed07e698c69df6da8744861c0ef7cd",
+    "url": "/gdg-shanghai-homepage/img/android.14ed07e6.svg"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/gdg-shanghai-homepage/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "1c973c35a927817f43a3ab8305fb9a48",
+    "url": "/gdg-shanghai-homepage/img/firebase.1c973c35.svg"
+  },
+  {
+    "revision": "19fe90bea7716a98539819c7e90ad390",
+    "url": "/gdg-shanghai-homepage/img/gdglogo.19fe90be.png"
+  },
+  {
+    "revision": "2e964ac1e80a82e9074025d47a1169b9",
+    "url": "/gdg-shanghai-homepage/index.html"
+  }
+];
